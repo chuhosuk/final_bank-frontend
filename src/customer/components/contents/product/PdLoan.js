df6bb4e7816f78b04acba4d5ccf9d359ffaf96aa
nuseyhgnajj <janghs0716@gmail.com>
2023-04-22 16:44:57 +0900

    jhs_test

--- src/customer/components/contents/product/PdLoan.js
+++ src/customer/components/contents/product/PdLoan.js
@@ -1,14 +1,12 @@
 // 대출 상품
 import React from "react";
 import pd_fudHaving from  '../../../../customer/resources/img/pdloan.png';
-import PdLoanDetail from './PdLoanDetail';
 
 function PdLoan () {
     return (
-       <div className="container">
-              <img src={pd_fudHaving} alt="" /><br />
+        <div className="container">
+             <img src={pd_fudHaving} alt="" /><br />
              SELECT * FROM loan_product;
-             <PdLoanDetail />
              <br />
              <br />
              <br />
